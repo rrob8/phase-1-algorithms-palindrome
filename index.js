cb63735e5fba11acdd8704c9693b760b9ebbcbb9
Robert Bubnik <rbubnik@gmail.com>
2023-08-29 22:04:59 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
+let TestWord = 'deed'
 function isPalindrome(word) {
-  // Write your algorithm here
+  
+  let value = ''
+  if (typeof word == 'string') {
+    let oldWord = []
+    let newWord = []
+    
+    for ( i=0; i<word.length; i++) {
+      oldWord.push(word[i])
+    }
+
+    
+    for (i= word.length -1; i>=0; i--) {
+      
+      newWord.push(word[i])
+      
+    }
+    console.log(oldWord)
+    console.log(newWord)
+    
+   value = compareWords( oldWord, newWord)
+
+  }
+ return value
 }
+
+function compareWords( oldWord, newWord) {
+if ( oldWord.toString() == newWord.toString()) {
+  return true
+}
+else {
+  return false
+}
+}
+
+
 
 /* 
   Add your pseudocode here
-*/
+1) function takes argument and checks if it is a string using typeof
+2) if yes, create an array version of the original word
+3) iterate through the original word and create an array that is the reverse order of the letters
+4) compare the two arrays
+5) run these arrays back to strings using toString() and compare them
+6) if they are equal, return true, if not, return false
+
+  */
 
 /*
   Add written explanation of your solution here
